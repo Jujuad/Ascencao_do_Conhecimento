73084efe3c975ed3baf26e39830b51dc7e5195c3
Jujuad <72460406+Jujuad@users.noreply.github.com>
2021-03-08 01:22:08 -0300

    Add files via upload

---
+++ nossaequipe.php
@@ -0,0 +1,68 @@
+<html>
+    <head>
+        <link rel = "stylesheet" type = "text/css" href="./css/main.css"> 
+
+        <title> Nossa Equipe</title>
+    </head>   
+        <header class = "menu">
+            <main>
+                <div class = "configuracao">
+                    <div class = "logo">
+                   <img src = "./img/logo.png">
+                    </div>
+                    <div class = "voltar">
+                      <a href="./index.html"><img src = "./img/back.png"></a>
+                  </div>
+                           </div>
+                <div id="myNav" class="overlay">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <div class="overlay-content">
+                       <a href="index.html">Home</a>
+                      <a href= "meuperfil.html">Meu Perfil</a>
+                      <a href="nossaequipe.html">Nossa Equipe</a>
+                    </div>
+                  
+                  </div>
+                  <span style=   "font-size:30px;cursor:pointer;padding: 5px;"onclick="openNav()">&#9776
+                </span>
+                  <script>
+                  function openNav() {
+                   document.getElementById("myNav").style.width = "100%";
+                 }
+                 
+                 function closeNav() {
+                   document.getElementById("myNav").style.width = "0%";
+                 }
+                 </script>
+                </main>
+                </header>
+                </style>
+                <body background="./img/background.png">
+                  <h1 style="color:rgb(0, 71, 73);" align="center">Nossa Equipe</h1>
+    <br></br>
+    <div class="equipe">
+      <h3 style="color:rgb(0, 0, 0);" text-align="justify"> Nossa equipe é formada por 4 estudantes de Informática 🖥️ do Instituto Federal de Educação, Ciência e tecnologia de São Paulo. 
+      Juntas tivemos a ideia de inserir os conhecimentos gerais de forma mais casual e interativa ao público, tendo nosso site por intermédio. Até porque... Quem não gosta de uma descontração para deixar os estudos mais divertidos e prazerosos não é mesmo?</h3>
+ <h3 style="color:rgb(0, 0, 0);" text-align="justify"> Nossas redes sociais estão disponíveis para contato, no caso de dúvidas. 
+      As mesmas também serão utilizadas como meio de comunicação, apresentando um entretenimento ativo</h3>
+    </div>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+
+      <center><a href= "mailto: ascensaodoconhecimento@gmail.com" id= "link" title =>Contate-nos aqui 📥</a></center>
+    
+    </body>
+                <footer>
+                
+                  <h4 style="color:black;" align = "center    "> Ana Caroline Borges, Beatriz Leandro Mazzeu, Giovanna Talliati Falcão, Julia Andrade Dias</h4>  </h5>
+                  <h3 style="color:black;" align="center">Instituto Federal</h3>   
+                                        </footer>
+                </html>
